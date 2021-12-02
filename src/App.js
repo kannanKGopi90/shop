c1088c400673366152f088a4530a2c488e7d4b75
kannan.gopi <kannan.gopi@gosite.com>
2021-12-02 12:41:04 +0530

    login rouf page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <h1>LOGIN</h1>
+      <div className="login_outer_div">
+        <div className="login_inner_div">
+        <input type="text" placeholder="user name"></input>
+        <br/>
+        <input type="text" placeholder="password"></input><br/>
+        </div>
+        <button>submit</button>
+      </div>
+      </>
     </div>
   );
 }
