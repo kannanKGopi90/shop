272707295a47d62f1876baed67d85aab399fd438
kannan <kannan.gopi.tblr@gmail.com>
2021-12-08 18:36:19 +0530

    firebase user authentication for fire store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import './App.css';
 import { Dashboard } from './components/dashboard';
 import { BrowserRouter,Router,Switch } from 'react-router-dom';
+import {useState} from 'react';
+import { collection,where,getDocs,query } from 'firebase/firestore';
+import db from './firebase/firebase';
+import { async } from '@firebase/util';
 
 function App() {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  
+  let handleClick =async()=>{
+    console.log("user",username);
+    let q =query(collection(db,"users"),where("username","==",username),where("password","==",password))
+    let snapshot =await getDocs(q)
+    snapshot.forEach(doc => {
+      console.log(doc.data());
+    });
+    
+  }
   return (
     <div className="App">
       <>
       <h1>LOGIN</h1>
       <div className="login_outer_div">
         <div className="login_inner_div">
-        <input type="text" placeholder="user name"></input>
+        <input type="text" placeholder="user name" onChange={(e)=>{setusername(e.target.value)}}></input>
         <br/>
-        <input type="text" placeholder="password"></input><br/>
+        <input type="text" placeholder="password" onChange={(e)=>{setpassword(e.target.value)}}></input><br/>
         </div>
-        <button>submit</button>
+        <button onClick={()=>handleClick()}>submit</button>
       </div>
       </>
       <Dashboard></Dashboard>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import "@firebase/storage"
+import { getFirestore } from "@firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,5 +15,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export let firestore = app.firestore()
+const adminApp = initializeApp(firebaseConfig);
+let db = getFirestore(adminApp);
+export default db;
